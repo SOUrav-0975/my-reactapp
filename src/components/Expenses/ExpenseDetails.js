db335b4361fa1d1d5a4d0dd5d7a388284396cdb9
SOUrav-0975 <souravk0975@gmail.com>
2022-11-11 15:03:11 +0530

    added changeAmount button

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,14 +1,23 @@
 
 import './ExpenseItem.css'
-
+import React,{useState} from "react"
 const ExpenseDetails = (props) => {
+const [amount,setAmount]= useState(props.amount)
+    const handleChange = () => {
+      setAmount('100')
+      console.log(amount)
+    }
     return(
         <div className="expense-item">
           <div className="expense-item__description">
                 <h2>{props.title}</h2>
-                
-                <div className="expense-item__price">${props.amount}</div>
+                </div>
+                <div className="expense-item__price">${amount}</div>
+           
+            <div>
+            <button onClick={handleChange}>Change Amount</button>
             </div>
+          
         </div>
     )
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,20 @@
 import "./ExpenseItem.css";
-
+import React,{useState} from 'react';
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
 const ExpenseItem = (props) => {
-  function deleteItem(){
+ /* function deleteItem(){
     console.log("deleted!!!")
-  }
+  }*/
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails title={props.title} amount={props.amount} />
-      <button onClick={deleteItem}>Delete</button>
+     
+    
+    
     </div>
   );
 }
